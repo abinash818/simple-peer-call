12ddbb46524ab2edf56ca108c791db19ed0e0765
abinash818 <63045717+abinash818@users.noreply.github.com>
2025-12-07 16:32:05 +0530

    Use environment variable for server port

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server running at http://0.0.0.0:${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
